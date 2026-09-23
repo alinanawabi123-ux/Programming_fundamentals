754478740b1d697aa5bc14843beeb409c3534903
Alina Nawabi <alinanawabi123@gmail.com>
2026-09-23 19:30:58 +0500

    Add logical operator to program

---
+++ C++ Basis/Practice/Logical operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age =  18;
+	bool hasId = true;
+	
+	cout<<"Using AND operator: "<<(age&&hasId)<<endl;
+	cout<<"Using OR operator: "<<(age||hasId)<<endl;
+	cout<<"Using NOR operator: "<<(!hasId)<<endl;
+
+	return 0;
+	
+}
